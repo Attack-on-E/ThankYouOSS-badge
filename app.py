a50f95a87d4790da167d60773a9e997f0856eafe
Hoshi Ran <ureshinostyle350@gmail.com>
2021-12-08 10:26:27 +0900

    [add] hello

--- app.py
+++ app.py
@@ -6,6 +6,11 @@ from src.shields import download_image
 app = Flask(__name__)
 
 
+@app.route('/')
+def hello():
+    return "hello"
+
+
 @app.route('/thank/<user_name>/<repo_name>.svg')
 def index(user_name: str = "", repo_name: str = ""):
     count = fetch_thank_count(user_name, repo_name)
